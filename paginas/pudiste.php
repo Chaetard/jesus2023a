225be24e4475709caf91ea8663454b3c71e8c294
Chaetard <jesus.santos4160@alumnos.udg.mx>
2023-05-08 08:55:32 -0600

    validaciones

--- paginas/pudiste.php
+++ paginas/pudiste.php
@@ -1,5 +1,13 @@
 <?php
 
-echo 'pudiste';
+if($_GET){
+    $combo_carreras=$_GET['combo_carreras'];
+    echo 'valor db'. $combo_carreras. '</br>';
+    //$txtnombre=$_GET['txtnombre'];
+    //echo 'tu nombre we';
+    //echo $txtnombre. '</br>';
+
+}
+echo 'creo que si jalo we';
 
 ?>
